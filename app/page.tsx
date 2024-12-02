2658b353d8aa9df6aedee906d8cf11031d00f4b8
Mukhammed <massalimov.mukhammed@mail.ru>
2024-12-02 11:34:19 +0500

    footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 
 export default function Home() {
    return (
       <div className="wrapper">
          <Header />
+         <main></main>
+         <Footer />
       </div>
    );
 }
---
+++ components/Footer.tsx
@@ -0,0 +1,69 @@
+import Link from "next/link";
+import Image from "next/image";
+import { Separator } from "@/components/ui/separator";
+
+const socials = [
+   {
+      name: "facebook",
+      url: "https://www.facebook.com/",
+      icon: "social/facebook.svg",
+   },
+   {
+      name: "twitter",
+      url: "https://x.com/home",
+      icon: "social/twitter.svg",
+   },
+   {
+      name: "linkedin",
+      url: "https://www.linkedin.com/feed/",
+      icon: "social/linkedin.svg",
+   },
+];
+
+export default function Footer() {
+   return (
+      <footer className="border-t border-black py-5 text-base sm:text-lg">
+         <div className="container flex max-w-[1596px] flex-col items-center justify-between gap-4 lg:flex-row">
+            <div className="flex flex-1 flex-col justify-center gap-[10px] text-center sm:flex-row sm:flex-wrap">
+               <Link href="/" className="hover:underline">
+                  Условия использования
+               </Link>
+               <Separator
+                  orientation="vertical"
+                  className="hidden h-[27px] bg-black sm:block"
+               />
+               <Link href="/" className="hover:underline">
+                  Политика конфиденциальности
+               </Link>
+            </div>
+            <Social />
+            <p className="flex flex-1 justify-end text-center">
+               &copy; {new Date().getFullYear()} Holy Melon. Все права защищены.
+            </p>
+         </div>
+      </footer>
+   );
+}
+
+function Social() {
+   return (
+      <ul className="flex items-center gap-[10px] rounded-full border border-[#142535] bg-[#142535]/[.93] p-[10px]">
+         {socials.map((social) => (
+            <li key={social.name}>
+               <a
+                  href={social.url}
+                  target="_blank"
+                  className="inline-flex size-12 items-center justify-center rounded-full bg-[#142535]"
+               >
+                  <Image
+                     src={social.icon}
+                     width={20}
+                     height={20}
+                     alt={social.name}
+                  />
+               </a>
+            </li>
+         ))}
+      </ul>
+   );
+}
