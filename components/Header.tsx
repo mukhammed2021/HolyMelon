8d1122ca2112f2a827a3c72160292462f8f39c4e
Mukhammed <massalimov.mukhammed@mail.ru>
2024-12-02 09:59:12 +0500

    header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,16 +1,137 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { ChevronDown, MenuIcon, Moon, SearchIcon, X } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import { DropdownMenu } from "@/components/ui/dropdown-menu";
+import {
+   DropdownMenu,
+   DropdownMenuContent,
+   DropdownMenuItem,
+   DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+   Dialog,
+   DialogTrigger,
+   DialogContent,
+   DialogHeader,
+   DialogTitle,
+   DialogClose,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import Search from "./Search";
+
+interface NavMenuProps {
+   isMenuOpen: boolean;
+}
+
+const cities = [
+   "Алматы",
+   "Астана (Нур-Султан)",
+   "Усть-Каменогорск (Öskemen)",
+   "Атырау",
+   "Шымкент",
+   "Баку (Азербайджан)",
+];
 
 export default function Header() {
+   const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+   useEffect(() => {
+      function handleOpen() {
+         if (window.outerWidth >= 1280) setIsMenuOpen(false);
+      }
+      window.addEventListener("resize", handleOpen);
+      return () => {
+         window.removeEventListener("resize", handleOpen);
+      };
+   }, []);
+
    return (
-      <header>
-         <div className="container">
-            <div>
+      <header className="fixed left-0 top-0 w-full bg-white">
+         <div className="container flex min-h-[6.25rem] items-center justify-between gap-x-4">
+            <div className="flex items-center gap-x-3 md:gap-x-5 lg:gap-x-8 xl:flex-1 xl:gap-x-12">
                <Logo />
+               <Separator
+                  orientation="vertical"
+                  className="z-[2] hidden h-[1.875rem] sm:block"
+               />
+               <Dialog>
+                  <DialogTrigger className="z-[2] hidden items-center font-bold uppercase sm:flex">
+                     Астана <ChevronDown />
+                  </DialogTrigger>
+                  <DialogContent className="max-h-screen overflow-auto xl:max-w-[71.25rem] xl:rounded-[2.5rem] xl:px-[5.9375rem] xl:py-[5.625rem]">
+                     <DialogHeader className="mb-[4.6875rem] flex-row items-center justify-between">
+                        <DialogTitle className="font-normal uppercase lg:text-[2rem]">
+                           выбрать город
+                        </DialogTitle>
+                        <DialogClose>
+                           <X size={50} />
+                        </DialogClose>
+                     </DialogHeader>
+                     <div className="relative">
+                        <SearchIcon
+                           size={30}
+                           className="absolute left-0 top-1/2 -translate-y-1/2"
+                        />
+                        <Input
+                           placeholder="Поиск"
+                           className="border-none pl-[2.625rem] placeholder:text-[#c6c6c6]/[.63] lg:text-[2rem]"
+                        />
+                     </div>
+                     <Separator className="mb-[4.75rem] mt-[.375rem]" />
+                     <ul className="space-y-[3.625rem] leading-none text-black/[.53] lg:text-[2rem]">
+                        {cities.map((city) => (
+                           <li key={city}>
+                              <button type="button">{city}</button>
+                           </li>
+                        ))}
+                     </ul>
+                  </DialogContent>
+               </Dialog>
             </div>
-            <nav></nav>
-            <div></div>
+            <NavMenu isMenuOpen={isMenuOpen} />
+            <div className="z-[2] flex items-center justify-end gap-x-[1.875rem] xl:flex-1">
+               <DropdownMenu>
+                  <DropdownMenuTrigger className="hidden font-bold uppercase xl:flex xl:items-center">
+                     о нас <ChevronDown />
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent className="rounded-[1.375rem] p-0">
+                     <DropdownMenuItem className="px-[1.375rem] py-4">
+                        <Link
+                           href="/staff"
+                           className="text-[1.375rem] font-bold uppercase"
+                        >
+                           сотрудники
+                        </Link>
+                     </DropdownMenuItem>
+                  </DropdownMenuContent>
+               </DropdownMenu>
+               <div className="flex items-center gap-4">
+                  <Search />
+                  <Button
+                     variant="icon"
+                     size="icon"
+                     className="bg-[#010e2a]/[.92] max-[375px]:size-10 max-[375px]:rounded-2xl"
+                  >
+                     <Moon className="text-white" />
+                  </Button>
+                  <button
+                     type="button"
+                     onClick={() => setIsMenuOpen((open) => !open)}
+                     className="max-[375px]:size-10 xl:hidden"
+                  >
+                     {isMenuOpen ? (
+                        <X size={50} className="max-[375px]:size-10" />
+                     ) : (
+                        <MenuIcon size={50} className="max-[375px]:size-10" />
+                     )}
+                  </button>
+               </div>
+            </div>
          </div>
       </header>
    );
@@ -18,8 +139,104 @@ export default function Header() {
 
 function Logo() {
    return (
-      <Link href="/">
+      <Link href="/" className="z-[2]">
          <Image src="/logo.png" width={120} height={74} alt="логотип" />
       </Link>
+   );
+}
+
+function NavMenu({ isMenuOpen }: NavMenuProps) {
+   return (
+      <nav
+         className={cn("hidden xl:block", {
+            "fixed left-0 top-0 block h-full w-full overflow-auto bg-white px-4 pb-4 pt-[6.25rem] before:fixed before:left-0 before:top-0 before:h-[6.25rem] before:w-full before:bg-white":
+               isMenuOpen,
+         })}
+      >
+         <ul
+            className={cn(
+               "flex items-center gap-x-[2.9375rem] font-bold uppercase",
+               {
+                  "flex-col gap-y-8": isMenuOpen,
+               },
+            )}
+         >
+            <li>
+               <Link href="/brands">бренды</Link>
+            </li>
+            <li>
+               <DropdownMenu>
+                  <DropdownMenuTrigger className="flex items-center font-bold uppercase">
+                     Фото и видео <ChevronDown />
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent className="rounded-[1.375rem] p-0">
+                     <DropdownMenuItem className="px-[1.375rem] py-4">
+                        <Link
+                           href="/reportage"
+                           className="text-[1.375rem] font-bold uppercase"
+                        >
+                           Репортажи
+                        </Link>
+                     </DropdownMenuItem>
+                  </DropdownMenuContent>
+               </DropdownMenu>
+            </li>
+            <li>
+               <Link href="/anonces">анонсы</Link>
+            </li>
+            <li className="block xl:hidden">
+               <DropdownMenu>
+                  <DropdownMenuTrigger className="flex font-bold uppercase xl:items-center">
+                     о нас <ChevronDown />
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent className="rounded-[1.375rem] p-0">
+                     <DropdownMenuItem className="px-[1.375rem] py-4">
+                        <Link
+                           href="/staff"
+                           className="text-[1.375rem] font-bold uppercase"
+                        >
+                           сотрудники
+                        </Link>
+                     </DropdownMenuItem>
+                  </DropdownMenuContent>
+               </DropdownMenu>
+            </li>
+            <li className="block sm:hidden">
+               <Dialog>
+                  <DialogTrigger className="z-[2] flex items-center font-bold uppercase">
+                     Астана <ChevronDown />
+                  </DialogTrigger>
+                  <DialogContent className="max-h-screen overflow-auto xl:max-w-[71.25rem] xl:rounded-[2.5rem] xl:px-[5.9375rem] xl:py-[5.625rem]">
+                     <DialogHeader className="mb-[4.6875rem] flex-row items-center justify-between">
+                        <DialogTitle className="font-normal uppercase lg:text-[2rem]">
+                           выбрать город
+                        </DialogTitle>
+                        <DialogClose>
+                           <X size={50} />
+                        </DialogClose>
+                     </DialogHeader>
+                     <div className="relative">
+                        <SearchIcon
+                           size={30}
+                           className="absolute left-0 top-1/2 -translate-y-1/2"
+                        />
+                        <Input
+                           placeholder="Поиск"
+                           className="border-none pl-[2.625rem] placeholder:text-[#c6c6c6]/[.63] lg:text-[2rem]"
+                        />
+                     </div>
+                     <Separator className="mb-[4.75rem] mt-[.375rem]" />
+                     <ul className="space-y-[3.625rem] leading-none text-black/[.53] lg:text-[2rem]">
+                        {cities.map((city) => (
+                           <li key={city}>
+                              <button type="button">{city}</button>
+                           </li>
+                        ))}
+                     </ul>
+                  </DialogContent>
+               </Dialog>
+            </li>
+         </ul>
+      </nav>
    );
 }
---
+++ components/Search.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { useEffect, useRef, useState } from "react";
+import { SearchIcon } from "lucide-react";
+import { Input } from "./ui/input";
+import { cn } from "@/lib/utils";
+
+export default function Search() {
+   const [isSearchOpen, setIsSearchOpen] = useState(false);
+   const searchRef = useRef<HTMLInputElement>(null!);
+
+   useEffect(() => {
+      if (isSearchOpen) searchRef.current.focus();
+   }, [isSearchOpen]);
+
+   return isSearchOpen ? (
+      <div
+         className={cn("relative z-[2] lg:w-[23.125rem]", {
+            "max-sm:fixed max-sm:left-0 max-sm:top-[6.25rem] max-sm:w-full":
+               isSearchOpen,
+         })}
+      >
+         <Input
+            onBlur={() => setIsSearchOpen(false)}
+            ref={searchRef}
+            placeholder="Введите запрос"
+            type="search"
+            className="rounded-[.3125rem] border-none bg-[#d9d9d9] py-3 pl-7 pr-[3.125rem] text-black placeholder:text-[#8b8585]"
+         />
+         <button
+            type="submit"
+            className="absolute inset-y-0 end-0 flex h-full w-[3.125rem] items-center justify-center rounded-e-[.3125rem] text-black ring-offset-background transition-shadow hover:text-foreground focus-visible:border focus-visible:border-ring focus-visible:text-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring/30 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50"
+         >
+            <SearchIcon />
+         </button>
+      </div>
+   ) : (
+      <button
+         type="submit"
+         onClick={() => setIsSearchOpen((s) => !s)}
+         className="z-[2] inline-flex size-[3.125rem] items-center justify-center max-[375px]:rounded-2xl rounded-[1.25rem] bg-[#E8E8E8] max-[375px]:size-10"
+      >
+         <SearchIcon size={25} aria-hidden="true" />
+      </button>
+   );
+}
