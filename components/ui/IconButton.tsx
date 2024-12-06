293821216a847726419fed08a1a33f47af74f9c6
Mukhammed <massalimov.mukhammed@mail.ru>
2024-12-06 15:27:56 +0500

    icon button

---
+++ components/ui/IconButton.tsx
@@ -0,0 +1,29 @@
+import { cn } from "@/lib/utils";
+
+interface IconButtonProps {
+   icon: React.ElementType;
+   size?: number;
+   iconClassName?: string;
+   className?: string;
+   href?: string;
+}
+
+export default function IconButton({
+   icon: Icon,
+   size = 40,
+   iconClassName,
+   className,
+   href = "",
+}: IconButtonProps) {
+   return (
+      <a
+         href={href}
+         className={cn(
+            "inline-flex size-[4.375rem] items-center justify-center rounded-full bg-neutral-100 transition-colors hover:bg-neutral-200",
+            className,
+         )}
+      >
+         <Icon size={size} className={iconClassName} />
+      </a>
+   );
+}
