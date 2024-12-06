5be4f17a72296e00b84d9d9b7a020390cd195898
Mukhammed <massalimov.mukhammed@mail.ru>
2024-12-06 15:28:32 +0500

    content header for all gallery pages

---
+++ components/ContentHeader.tsx
@@ -0,0 +1,72 @@
+import { Camera, Download, Forward } from "lucide-react";
+import {
+   Breadcrumb,
+   BreadcrumbItem,
+   BreadcrumbLink,
+   BreadcrumbList,
+   BreadcrumbPage,
+   BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import IconButton from "@/components/ui/IconButton";
+
+interface ContentHeaderProps {
+   title: string;
+   downloadLink: string;
+}
+
+export default function ContentHeader({
+   title,
+   downloadLink,
+}: ContentHeaderProps) {
+   return (
+      <>
+         <Breadcrumb className="mb-10">
+            <BreadcrumbList className="uppercase text-black sm:text-base">
+               <BreadcrumbItem>
+                  <BreadcrumbLink
+                     href="/"
+                     className="font-bold hover:underline"
+                  >
+                     главная страница
+                  </BreadcrumbLink>
+               </BreadcrumbItem>
+               <BreadcrumbSeparator>/</BreadcrumbSeparator>
+               <BreadcrumbItem>
+                  <BreadcrumbPage className="font-bold">{title}</BreadcrumbPage>
+               </BreadcrumbItem>
+            </BreadcrumbList>
+         </Breadcrumb>
+         <div className="mb-16 flex flex-col justify-between gap-5 min-[830px]:flex-row lg:mb-24 lg:items-center">
+            <div>
+               <h1 className="mb-[.625rem] text-3xl font-bold uppercase sm:text-4xl lg:text-5xl">
+                  {title}
+               </h1>
+               <p className="text-lg capitalize lg:text-xl">
+                  <time dateTime="09-05">5 Сентября</time>, Астана
+               </p>
+            </div>
+            <div className="flex flex-wrap items-center justify-between gap-x-12 gap-y-5 lg:gap-x-[4.375rem]">
+               <div className="flex items-center gap-x-[1.375rem]">
+                  <Camera className="size-11 lg:size-[3.4375rem]" />
+                  <p className="text-lg font-bold uppercase lg:text-xl">
+                     фотограф <br /> иванович иван
+                  </p>
+               </div>
+               <div className="flex items-center gap-6">
+                  <IconButton
+                     icon={Forward}
+                     iconClassName="size-8 lg:size-10"
+                     className="size-14 lg:size-[4.375rem]"
+                  />
+                  <IconButton
+                     icon={Download}
+                     iconClassName="size-8 lg:size-10"
+                     className="size-14 lg:size-[4.375rem]"
+                     href={downloadLink}
+                  />
+               </div>
+            </div>
+         </div>
+      </>
+   );
+}
