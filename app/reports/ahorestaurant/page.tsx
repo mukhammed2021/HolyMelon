51b2c175ce198c7537d4a2373098a9f1dafc1625
Mukhammed <massalimov.mukhammed@mail.ru>
2024-12-11 17:14:40 +0500

    aho gallery

---
+++ app/reports/ahorestaurant/page.tsx
@@ -0,0 +1,29 @@
+import ContentHeader from "@/components/ContentHeader";
+import Footer from "@/components/Footer";
+import Gallery from "@/components/Gallery";
+import Header from "@/components/Header";
+import { getData } from "@/api/api";
+
+export default async function AhoPage() {
+   const data = await getData("aho restaurant");
+   const { title, photos, downloadObj, created } = data;
+
+   return (
+      <div className="wrapper">
+         <Header />
+         <main>
+            <section className="pt-[9.375rem] md:pt-[12.375rem]">
+               <div className="container">
+                  <ContentHeader
+                     title={title}
+                     downloadLink={downloadObj.href}
+                     created={created}
+                  />
+                  <Gallery photos={photos} />
+               </div>
+            </section>
+         </main>
+         <Footer />
+      </div>
+   );
+}
