96457859def418a16e8ea6d53710cbb28cb82fe7
Mukhammed <massalimov.mukhammed@mail.ru>
2024-12-17 12:49:40 +0500

    anonce

---
+++ app/anonces/ahorestaurant/Anonce.tsx
@@ -0,0 +1,50 @@
+import { Download, Forward } from "lucide-react";
+import IconButton from "@/components/ui/IconButton";
+import Image from "next/image";
+
+export default function Anonce() {
+   return (
+      <div className="flex flex-col-reverse items-center justify-between gap-7 lg:flex-row">
+         <div className="lg:basis-[44.5rem]">
+            <h1 className="mb-10 text-4xl uppercase sm:text-5xl md:text-7xl lg:mb-[5.8125rem] 2xl:text-8xl">
+               aho restaurant
+            </h1>
+            <div className="mb-10 space-y-10 text-xl lg:mb-[4.5625rem] 2xl:text-2xl">
+               <div className="space-y-6">
+                  <p className="text-[#676768]">Дата проведения:</p>
+                  <time dateTime="2024-09-05" className="block">
+                     5 Сентября, 2024
+                  </time>
+               </div>
+               <div className="space-y-6">
+                  <p className="text-[#676768]">Анонс:</p>
+                  <p>
+                     Улица Ленина, дом 123, квартира 45 Москва, Россия, 101000
+                  </p>
+               </div>
+            </div>
+            <div className="flex flex-wrap items-center gap-x-[3.3125rem] gap-y-6">
+               <span className="text-xl 2xl:text-2xl">Поделиться</span>
+               <div className="flex items-center gap-x-6">
+                  <IconButton icon={Forward} />
+                  <IconButton icon={Download} />
+               </div>
+            </div>
+         </div>
+         <div>
+            <div className="relative">
+               <Image
+                  src="/anoncesPage/aho/01.jpeg"
+                  width={855}
+                  height={702}
+                  alt="aho"
+                  className="aspect-[855/702] size-full rounded-[1.25rem]"
+               />
+               <p className="absolute left-5 top-8 rounded-[1.25rem] bg-[#212020]/[.7] px-10 py-[.4375rem] text-white sm:left-9 sm:top-[50px] sm:px-[5.25rem] sm:text-2xl">
+                  Астана
+               </p>
+            </div>
+         </div>
+      </div>
+   );
+}
---
+++ app/anonces/ahorestaurant/page.tsx
@@ -0,0 +1,47 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import {
+   Breadcrumb,
+   BreadcrumbItem,
+   BreadcrumbLink,
+   BreadcrumbList,
+   BreadcrumbPage,
+   BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import Anonce from "./Anonce";
+
+export default function page() {
+   return (
+      <div className="wrapper">
+         <Header />
+         <main>
+            <Hero />
+            <section className="pb-[4.375rem] md:pb-28 lg:pb-[8.75rem]">
+               <div className="container">
+                  <Breadcrumb className="mb-10 sm:mb-[3.5625rem]">
+                     <BreadcrumbList className="uppercase text-black sm:text-base">
+                        <BreadcrumbItem>
+                           <BreadcrumbLink
+                              href="/"
+                              className="font-bold hover:underline"
+                           >
+                              главная страница
+                           </BreadcrumbLink>
+                        </BreadcrumbItem>
+                        <BreadcrumbSeparator>/</BreadcrumbSeparator>
+                        <BreadcrumbItem>
+                           <BreadcrumbPage className="font-bold">
+                              анонсы
+                           </BreadcrumbPage>
+                        </BreadcrumbItem>
+                     </BreadcrumbList>
+                  </Breadcrumb>
+                  <Anonce />
+               </div>
+            </section>
+         </main>
+         <Footer />
+      </div>
+   );
+}
