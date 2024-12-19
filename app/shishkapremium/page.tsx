627bf55ca509b16a266b2c36859d8c49c07e681b
Mukhammed <massalimov.mukhammed@mail.ru>
2024-12-19 14:46:50 +0500

    shishka premium page

---
+++ app/shishkapremium/page.tsx
@@ -0,0 +1,29 @@
+import { getData } from "@/api/api";
+import ContentHeader from "@/components/ContentHeader";
+import Footer from "@/components/Footer";
+import Gallery from "@/components/Gallery";
+import Header from "@/components/Header";
+
+export default async function ShishkaPremiumPage() {
+   const data = await getData("shishka premium");
+   const { title, photos, downloadObj, created } = data;
+
+   return (
+      <div className="wrapper">
+         <Header />
+         <main>
+            <section className="pt-[9.375rem] md:pt-[12.375rem]">
+               <div className="container">
+                  <ContentHeader
+                     title={title}
+                     downloadLink={downloadObj.href}
+                     created={created}
+                  />
+                  <Gallery photos={photos} />
+               </div>
+            </section>
+         </main>
+         <Footer />
+      </div>
+   );
+}
