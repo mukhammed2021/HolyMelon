021b866c42b54985f9107ffab663adb8a19cd354
Mukhammed <massalimov.mukhammed@mail.ru>
2024-12-25 17:17:32 +0500

    update staff component

--- app/staff/Staff.tsx
+++ app/staff/Staff.tsx
@@ -1,4 +1,10 @@
 import Image from "next/image";
+import {
+   Accordion,
+   AccordionContent,
+   AccordionItem,
+   AccordionTrigger,
+} from "@/components/ui/accordion";
 
 const members = [
    {
@@ -55,28 +61,201 @@ export default function Staff() {
    return (
       <section className="pb-10 pt-7 sm:pb-24 md:pb-48 md:pt-14">
          <div className="container">
-            <h2 className="mb-8 text-3xl font-bold uppercase sm:text-[2.6875rem] md:mb-28">
-               Сотрудники
+            <h2 className="mb-7 text-3xl font-bold uppercase sm:text-[2.6875rem] md:mb-[2.5625rem]">
+               команда
             </h2>
-            <div className="grid grid-cols-[repeat(auto-fit,minmax(18rem,1fr))] gap-6 text-center min-[480px]:grid-cols-[repeat(auto-fit,minmax(20.625rem,1fr))] sm:gap-y-[4.5rem]">
-               {members.map((member) => (
-                  <div key={member.id}>
-                     <div className="mb-4 sm:mb-[2.125rem]">
-                        <Image
-                           src={member.src}
-                           width={417}
-                           height={503}
-                           className="size-full rounded-[1.25rem] object-cover max-[480px]:h-[350px] sm:aspect-[417/503]"
-                           alt={member.fullName}
-                        />
-                     </div>
-                     <h3 className="text-xl sm:text-[1.75rem]">
-                        {member.fullName}
-                     </h3>
-                     <p className="text-lg sm:text-2xl">{member.position}</p>
-                  </div>
-               ))}
+            <div className="mb-10 space-y-4 text-2xl md:mb-12 lg:mb-24">
+               <p>Команда — наша душа, наш успех.</p>
+               <p>
+                  В каждом проекте, в каждой детали и в каждом достижении есть
+                  одна важная составляющая — люди. В Holy Melon Management мы
+                  уверены, что настоящая сила компании заключается в её команде.
+               </p>
             </div>
+            <Accordion type="multiple">
+               <AccordionItem value="item-1" className="border-none">
+                  <AccordionTrigger className="uppercase">
+                     aho restaurant
+                  </AccordionTrigger>
+                  <AccordionContent>
+                     <div className="grid auto-cols-[9.8125rem] grid-cols-1 gap-6 md:grid-cols-2 min-[1440px]:grid-cols-3">
+                        {members.map((member) => (
+                           <div className="flex gap-5 lg:gap-10">
+                              <div>
+                                 <Image
+                                    src={member.src}
+                                    width={147}
+                                    height={157}
+                                    alt={member.fullName}
+                                    className="aspect-[147/157] object-cover"
+                                 />
+                              </div>
+                              <div className="space-y-[.4375rem] text-sm lg:text-xl xl:text-2xl">
+                                 <h4>{member.fullName}</h4>
+                                 <p className="font-light">{member.position}</p>
+                              </div>
+                           </div>
+                        ))}
+                     </div>
+                  </AccordionContent>
+               </AccordionItem>
+               <AccordionItem value="item-2" className="border-none">
+                  <AccordionTrigger className="uppercase">
+                     Shishka Botanic
+                  </AccordionTrigger>
+                  <AccordionContent>
+                     <div className="grid auto-cols-[9.8125rem] grid-cols-1 gap-6 md:grid-cols-2 min-[1440px]:grid-cols-3">
+                        {members.map((member) => (
+                           <div className="flex gap-5 lg:gap-10">
+                              <div>
+                                 <Image
+                                    src={member.src}
+                                    width={147}
+                                    height={157}
+                                    alt={member.fullName}
+                                    className="aspect-[147/157] object-cover"
+                                 />
+                              </div>
+                              <div className="space-y-[.4375rem] text-sm lg:text-xl xl:text-2xl">
+                                 <h4>{member.fullName}</h4>
+                                 <p className="font-light">{member.position}</p>
+                              </div>
+                           </div>
+                        ))}
+                     </div>
+                  </AccordionContent>
+               </AccordionItem>
+               <AccordionItem value="item-3" className="border-none">
+                  <AccordionTrigger className="uppercase">
+                     SHISHKA HIGHVILL
+                  </AccordionTrigger>
+                  <AccordionContent>
+                     <div className="grid auto-cols-[9.8125rem] grid-cols-1 gap-6 md:grid-cols-2 min-[1440px]:grid-cols-3">
+                        {members.map((member) => (
+                           <div className="flex gap-5 lg:gap-10">
+                              <div>
+                                 <Image
+                                    src={member.src}
+                                    width={147}
+                                    height={157}
+                                    alt={member.fullName}
+                                    className="aspect-[147/157] object-cover"
+                                 />
+                              </div>
+                              <div className="space-y-[.4375rem] text-sm lg:text-xl xl:text-2xl">
+                                 <h4>{member.fullName}</h4>
+                                 <p className="font-light">{member.position}</p>
+                              </div>
+                           </div>
+                        ))}
+                     </div>
+                  </AccordionContent>
+               </AccordionItem>
+               <AccordionItem value="item-4" className="border-none">
+                  <AccordionTrigger className="uppercase">
+                     Shishka Alpha
+                  </AccordionTrigger>
+                  <AccordionContent>
+                     <div className="grid auto-cols-[9.8125rem] grid-cols-1 gap-6 md:grid-cols-2 min-[1440px]:grid-cols-3">
+                        {members.map((member) => (
+                           <div className="flex gap-5 lg:gap-10">
+                              <div>
+                                 <Image
+                                    src={member.src}
+                                    width={147}
+                                    height={157}
+                                    alt={member.fullName}
+                                    className="aspect-[147/157] object-cover"
+                                 />
+                              </div>
+                              <div className="space-y-[.4375rem] text-sm lg:text-xl xl:text-2xl">
+                                 <h4>{member.fullName}</h4>
+                                 <p className="font-light">{member.position}</p>
+                              </div>
+                           </div>
+                        ))}
+                     </div>
+                  </AccordionContent>
+               </AccordionItem>
+               <AccordionItem value="item-5" className="border-none">
+                  <AccordionTrigger className="uppercase">
+                     Shishka Republic
+                  </AccordionTrigger>
+                  <AccordionContent>
+                     <div className="grid auto-cols-[9.8125rem] grid-cols-1 gap-6 md:grid-cols-2 min-[1440px]:grid-cols-3">
+                        {members.map((member) => (
+                           <div className="flex gap-5 lg:gap-10">
+                              <div>
+                                 <Image
+                                    src={member.src}
+                                    width={147}
+                                    height={157}
+                                    alt={member.fullName}
+                                    className="aspect-[147/157] object-cover"
+                                 />
+                              </div>
+                              <div className="space-y-[.4375rem] text-sm lg:text-xl xl:text-2xl">
+                                 <h4>{member.fullName}</h4>
+                                 <p className="font-light">{member.position}</p>
+                              </div>
+                           </div>
+                        ))}
+                     </div>
+                  </AccordionContent>
+               </AccordionItem>
+               <AccordionItem value="item-6" className="border-none">
+                  <AccordionTrigger className="uppercase">
+                     Shishka Premium
+                  </AccordionTrigger>
+                  <AccordionContent>
+                     <div className="grid auto-cols-[9.8125rem] grid-cols-1 gap-6 md:grid-cols-2 min-[1440px]:grid-cols-3">
+                        {members.map((member) => (
+                           <div className="flex gap-5 lg:gap-10">
+                              <div>
+                                 <Image
+                                    src={member.src}
+                                    width={147}
+                                    height={157}
+                                    alt={member.fullName}
+                                    className="aspect-[147/157] object-cover"
+                                 />
+                              </div>
+                              <div className="space-y-[.4375rem] text-sm lg:text-xl xl:text-2xl">
+                                 <h4>{member.fullName}</h4>
+                                 <p className="font-light">{member.position}</p>
+                              </div>
+                           </div>
+                        ))}
+                     </div>
+                  </AccordionContent>
+               </AccordionItem>
+               <AccordionItem value="item-7" className="border-none">
+                  <AccordionTrigger className="uppercase">
+                     SHISHKA EDITION
+                  </AccordionTrigger>
+                  <AccordionContent>
+                     <div className="grid auto-cols-[9.8125rem] grid-cols-1 gap-6 md:grid-cols-2 min-[1440px]:grid-cols-3">
+                        {members.map((member) => (
+                           <div className="flex gap-5 lg:gap-10">
+                              <div>
+                                 <Image
+                                    src={member.src}
+                                    width={147}
+                                    height={157}
+                                    alt={member.fullName}
+                                    className="aspect-[147/157] object-cover"
+                                 />
+                              </div>
+                              <div className="space-y-[.4375rem] text-sm lg:text-xl xl:text-2xl">
+                                 <h4>{member.fullName}</h4>
+                                 <p className="font-light">{member.position}</p>
+                              </div>
+                           </div>
+                        ))}
+                     </div>
+                  </AccordionContent>
+               </AccordionItem>
+            </Accordion>
          </div>
       </section>
    );
