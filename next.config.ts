45ccdfa4d0c7749f9424a1bc942a969c288f3c5a
Mukhammed <massalimov.mukhammed@mail.ru>
2024-12-27 16:01:44 +0500

    configure headers referrer policy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+   /* config options here */
+   async headers() {
+      return [
+         {
+            source: "/(.*)",
+            headers: [
+               {
+                  key: "Referrer-Policy",
+                  value: "no-referrer",
+               },
+            ],
+         },
+      ];
+   },
 };
 
 export default nextConfig;
